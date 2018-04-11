b7e63e94afc882fa248d971a82de3256f5e7f114
Olivier Calpetard <35366861+oliviercalpetard@users.noreply.github.com>
2018-04-11 10:37:57 +0400

    Add files via upload

--- modifie_groupe_esu.sh
+++ modifie_groupe_esu.sh
@@ -9,6 +9,12 @@ then
   exit 
 fi 
 
+#determiner le repertoire de lancement
+updatedb
+locate version_esubuntu.txt > files_tmp
+sed -i -e "s/version_esubuntu.txt//g" files_tmp
+read chemin < files_tmp
+echo $chemin
 
 #salle du pc
 echo "Entrez le groupe ESU (salle): "
@@ -17,7 +23,7 @@ read salle
 echo "$salle" > /etc/GM_ESU
 
 #on lance le script prof_firefox en mode sudo 
-sudo ./firefox/prof_firefox.sh
+sudo "$chemin"firefox/prof_firefox.sh
 
 echo "C'est fini ! bienvenue dans le groupe $salle..."
 exit
