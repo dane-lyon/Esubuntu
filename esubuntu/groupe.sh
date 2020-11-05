037ff40a196a512c2bb65a3fcdd231a3da936ead
Olivier Calpetard <35366861+oliviercalpetard@users.noreply.github.com>
2020-11-05 08:25:06 +0400

    Update
    Ajout possibilité de modifier le groupe machine via un fichier csv

--- esubuntu/groupe.sh
+++ esubuntu/groupe.sh
@@ -2,8 +2,10 @@
 
 #### utilitaire pour upkg ####
 # - récupère la valeur du groupe et execute upkg_client si 
-# - ver 2.0.1
-# - 20 mars 2018
+# - récupère le groupe machine depuis une liste csv dans r:\icones et modifie le groupe du pc
+# nul si le fichier liste_pc_esu.csv n'existe pas
+# - ver 2.0.3
+# - 05 novembre 2020
 # - CALPETARD Olivier
 
 
@@ -17,10 +19,52 @@ groupe=$GROUPS
 if [ groupe=10000 ] || [ groupe=10001 ] || [ groupe=10002 ]
 then
 echo "ok"
+
+ #controle de la présence du fichier liste_pc.csv dans icones qui sert de référence 
+ 	echo "controle presense liste_pc_esu.csv dans icones";
+ 	if [ -f /tmp/netlogon/icones/liste_pc_esu.csv ]
+	then 
+	echo "La liste des pc existe"
+	#recherche nom du pc et son groupe dans le fichier
+
+	salle=$(grep $HOSTNAME /tmp/netlogon/icones/liste_pc_esu.csv | awk -F';' ' {gsub("\r","",$2); printf $2 } ' )
+	
+	if [ -z "$salle" ]
+	then
+	echo "le pc n'exite pas dans la liste"
+	salle=grp_eole
+	else
+	salle=$salle
+	fi
+
+	gm_esu=$(cat /etc/GM_ESU)
+
+		if [ $gm_esu != $salle ]
+		then
+
+		echo "$salle"
+		echo "$salle" > /etc/GM_ESU
+		sudo rm /usr/local/upkg_client/stamp.date
+
+#firefox
+
+gm_esu=$(cat /etc/GM_ESU)
+echo '//
+lockPref("autoadmin.global_config_url", "file:///tmp/netlogon/icones/'$gm_esu'/linux/firefox.js"); '> /usr/lib/firefox/firefox.cfg
+		else
+		echo meme groupe on ne fait rien
+		fi
+	else 
+	echo "La liste des pc n'existe pas"
+	#on ne tiens pas compte des groupes esu par rapport au fichier
+	fi
+#execution de upkg
+
  sudo sh /etc/esubuntu/upkg_client.sh
 else 
+
 echo  "pas ok"
 	exit 0
 fi
 
-#exit 0
+exit 0
